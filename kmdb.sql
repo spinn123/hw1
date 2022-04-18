2e18d8e5bb8a0bda0a3fdb6144c96c507710863e
spinn123 <sarah.pinner@kellogg.northwestern.edu>
2022-04-18 13:03:52 +0000

    Drop tables and create tables

--- kmdb.sql
+++ kmdb.sql
@@ -102,12 +102,46 @@
 -- Drop existing tables, so you'll start fresh each time this script is run.
 -- TODO!
 
+DROP TABLE IF EXISTS movies;
+DROP TABLE IF EXISTS actors;
+DROP TABLE IF EXISTS studios;
+DROP TABLE IF EXISTS roles;
+
 -- Create new tables, according to your domain model
 -- TODO!
+
+
+CREATE TABLE actors (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    first_name TEXT,
+    last_name TEXT
+);
+
+CREATE TABLE movies (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT,
+    rating TEXT,
+    release_date TEXT,
+    studio_id
+);
+
+CREATE TABLE studios (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT
+);
+
+CREATE TABLE roles (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    role TEXT,
+    movie_id TEXT,
+    actor_id TEXT
+);
 
 -- Insert data into your database that reflects the sample data shown above
 -- Use hard-coded foreign key IDs when necessary
 -- TODO!
+
+
 
 -- Prints a header for the movies output
 .print "Movies"
